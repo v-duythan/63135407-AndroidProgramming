ec2b7815be005ac8ca2706e6dcaf9d151e4d20c7
v-duythan <vduythan@gmail.com>
2024-03-24 22:35:43 +0700

    Thêm mới bài 3

---
+++ Cau3_GioiThieu/app/src/main/java/ntu/vdt63135407/cau3_gioithieu/MainActivity.java
@@ -0,0 +1,14 @@
+package ntu.vdt63135407.cau3_gioithieu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
