13118152db91dd05e1ef044173cd4851f0af162b
v-duythan <vduythan@gmail.com>
2024-04-26 14:23:34 +0700

    Update

--- UsingBottomNavigationView/app/src/main/java/ntu/vdt63135407/usingbottomnavigationview/MainActivity.java
+++ UsingBottomNavigationView/app/src/main/java/ntu/vdt63135407/usingbottomnavigationview/MainActivity.java
@@ -1,8 +1,10 @@
 package ntu.vdt63135407.usingbottomnavigationview;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        // as soon as the application opens the first
+        // fragment should be shown to the user
+        // in this case it is algorithm fragment
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
     }
+
+    private final BottomNavigationView.OnNavigationItemSelectedListener navListener = item -> {
+        // By using switch we can easily get
+        // the selected fragment
+        // by using there id.
+        Fragment selectedFragment = null;
+        int itemId = item.getItemId();
+        if (itemId == R.id.algorithm) {
+            selectedFragment = new HomeFragment();
+        } else if (itemId == R.id.course) {
+            selectedFragment = new MenuFragment();
+        } else if (itemId == R.id.profile) {
+            selectedFragment = new ProfileFragment();
+        }
+        // It will help to replace the
+        // one fragment to other.
+        if (selectedFragment != null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+        }
+        return true;
+    };
 }
